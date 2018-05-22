3e4172a5610470502069d955fa349fbd8a41f8d2
john <nergilz@yandex.ru>
2018-05-22 03:27:12 +0200

    first version

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# Night Owls Detector
+## Night Owls Detector
 
-[TODO. There will be project description]
+---
+
+### Discription
+
++ The script find out who sent the tasks for verification after 24:00
++ You need a [devman API](https://devman.org/api/challenges/solution_attempts/?page=1)
++ You need a library [requests](http://docs.python-requests.org/en/master/user/quickstart/)
++ and library [pytz](http://pytz.sourceforge.net/)
++ It is recommended to use [virtualenv](https://docs.python.org/3/library/venv.html) 
+
+
+### How to install requests
+
+```bash
+pip install requests  # or pip3 
+```
+
+### How to install pytz
+
+```bash
+pip install pytz  # or pip3 
+```
+
+### Start script example
+
+```bash
+python3 seek_dev_nighters.py
+```
+
+
+### Requirements
+
+```bash
+Python ver 3.5 (or higher)
+```
+---
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,65 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
+import timeit
+import sys
 
-def get_midnighters():
-    pass
+
+def load_attempts(dev_url):
+
+    pages = requests.get(
+        dev_url,
+        {'page': 1}
+        ).json()['number_of_pages']
+
+    for page in range(1, int(pages)):
+
+        yield requests.get(
+            dev_url,
+            {'page': page}
+            ).json()['records']
+
+
+def get_local_user_time(data_user):
+
+    time_utc = datetime.fromtimestamp(data_user['timestamp'])
+    time_zone = pytz.timezone(data_user['timezone'])
+    return time_zone.localize(time_utc)
+
+
+def get_midnighters(response):
+    midnighters = []
+    time_form = '%H'
+    before = 0
+    after = 6
+
+    for page in response:
+        for user in page:
+            user_name = user['username']
+            local_user_time = get_local_user_time(user)
+
+            if before < int(datetime.strftime(local_user_time, time_form)) < after:
+                midnighters.append(user_name)
+    return set(midnighters)
+
+
+def pprint_night_owls(midnighters):
+
+    for user_name in midnighters:
+        print(user_name)
+
 
 if __name__ == '__main__':
-  pass
+    start_time = timeit.default_timer()
+    dev_url = 'https://devman.org/api/challenges/solution_attempts/'
+
+    try:
+        response = load_attempts(dev_url)
+
+    except requests.HTTPError as error:
+        sys.exit('ERROR: {}'.format(error))
+
+    midnighters = get_midnighters(response)
+    pprint_night_owls(midnighters)
+
+    print(' Time script: {}'.format(timeit.default_timer() - start_time))
